1867142565ef6da3d34a9b11f071e3a02267e614
Ying Wang <ying.wangthoughtworks.com@Yings-MacBook-Pro.local>
2021-04-10 16:30:14 +0800

    [wangying] feat: able to load images.

--- app/src/main/java/com/example/wechatclone/MainActivity.kt
+++ app/src/main/java/com/example/wechatclone/MainActivity.kt
@@ -1,13 +1,11 @@
 package com.example.wechatclone
 
 import android.os.Bundle
-import android.util.Log
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.network.Endpoints
 import com.example.network.ServiceBuilder
-import com.example.wechatclone.data.AllTweets
 import com.example.wechatclone.data.Tweet
 import kotlinx.android.synthetic.main.activity_main.*
 import retrofit2.Call
--- app/src/main/java/com/example/wechatclone/TweetViewHolder.kt
+++ app/src/main/java/com/example/wechatclone/TweetViewHolder.kt
@@ -1,12 +1,15 @@
 package com.example.wechatclone
 
 import android.view.View
+import android.widget.GridView
 import androidx.recyclerview.widget.RecyclerView
 import com.bumptech.glide.Glide
 import com.example.wechatclone.data.Tweet
 import kotlinx.android.synthetic.main.fragment_tweet.view.*
 
 class TweetViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+
+    private var playerImages = intArrayOf(R.drawable.ronaldo, R.drawable.felix, R.drawable.bernado, R.drawable.andre)
 
     fun bind(tweet: Tweet) {
         if (tweet.error.isNullOrEmpty() && tweet.unknownError.isNullOrEmpty()) {
@@ -16,6 +19,13 @@ class TweetViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
                 .into(itemView.avatar)
             itemView.user_name.text = tweet.sender.userName
             itemView.content_text_view.text = tweet.content
+
+            val gridView = itemView.findViewById<GridView>(R.id.grid_view)
+            val mainAdapter = itemView.context?.let { ImageAdapter(it, playerImages) }
+            if (gridView != null) {
+                gridView.adapter = mainAdapter
+            }
+
         }
     }
 }
