a13650c7affae9a725fefc1c5aa3b5af6a5578c7
Ying Wang <ying.wangthoughtworks.com@Yings-MacBook-Pro.local>
2021-04-10 16:56:50 +0800

    [wangying] feat: able to load images but only one line.

--- app/src/main/java/com/example/wechatclone/Endpoints.kt
+++ app/src/main/java/com/example/wechatclone/Endpoints.kt
@@ -1,11 +1,8 @@
 package com.example.network
 
-import com.example.wechatclone.data.AllTweets
 import com.example.wechatclone.data.Tweet
 import retrofit2.Call
 import retrofit2.http.GET
-import retrofit2.http.Query
-import retrofit2.http.Url
 
 interface Endpoints {
 
