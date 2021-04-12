3632fae8e5641bd93ffce475a94f2fc7acfcba3a
Ying Wang <ying.wangthoughtworks.com@Yings-MacBook-Pro.local>
2021-04-12 13:35:49 +0800

    add 1 line.

--- app/src/main/java/com/example/wechatclone/TweetViewHolder.kt
+++ app/src/main/java/com/example/wechatclone/TweetViewHolder.kt
@@ -14,6 +14,7 @@ import java.util.HashMap
 
 class TweetViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
 
+
     private var mCircleGridView: GridView? = null
 
     fun bind(tweet: Tweet) {
