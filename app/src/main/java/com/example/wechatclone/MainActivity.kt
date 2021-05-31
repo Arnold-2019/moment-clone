f296bf684835a2c09a7652a8dcc67bd655fe5dbc
ying <ying.wang01@student.adelaide.edu.au>
2021-05-31 22:51:43 +0800

    feat: drop down refreshing.

--- app/src/main/java/com/example/wechatclone/MainActivity.kt
+++ app/src/main/java/com/example/wechatclone/MainActivity.kt
@@ -5,6 +5,7 @@ import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
 import androidx.lifecycle.Observer
 import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.swiperefreshlayout.widget.SwipeRefreshLayout
 import com.example.wechatclone.ui.MomentRecyclerViewAdapter
 import com.example.wechatclone.ui.MomentViewModel
 import dagger.hilt.android.AndroidEntryPoint
@@ -13,11 +14,14 @@ import kotlinx.android.synthetic.main.activity_main.*
 @AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
 
+    lateinit var swipeRefreshLayout: SwipeRefreshLayout
     private val viewModel by viewModels<MomentViewModel>()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        swipeRefreshLayout = findViewById(R.id.swipeRefreshLayout)
 
         val adapter = MomentRecyclerViewAdapter()
         recycler_view.apply {
@@ -31,6 +35,13 @@ class MainActivity : AppCompatActivity() {
             })
             getTweets()
             getUserProfile()
+        }
+
+        swipeRefreshLayout.setOnRefreshListener {
+            viewModel.getUserProfile()
+            viewModel.getTweets()
+            adapter.refreshPage(viewModel.profile.value!!, viewModel.tweets.value!!)
+            swipeRefreshLayout.isRefreshing = false
         }
     }
 }
